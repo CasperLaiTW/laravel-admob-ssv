b21e53d3ef13a962adb642f8896a6cc30a1a0c7e
Casper Lai <casper.lai@sleepingdesign.com>
2021-01-08 20:18:35 +0800

    feat: add cache driver

--- src/PublicKey.php
+++ src/PublicKey.php
@@ -5,6 +5,7 @@ namespace Casperlaitw\LaravelAdmobSsv;
 use Eastwest\Json\Json;
 use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
@@ -30,6 +31,11 @@ class PublicKey
      * @var array
      */
     private $keyMap = [];
+
+    /**
+     * @var
+     */
+    public static $cacheThroughCallback;
 
     /**
      * PublicKey constructor.
@@ -61,7 +67,7 @@ class PublicKey
      */
     public function fetchPublicKeys()
     {
-        $client = new Client();
+        $client = new Client($this->buildOptions());
         $response = $client->request('GET', $this->publicKeyUrl);
 
         return $this->keyMap = Json::decode($response->getBody()->getContents(), true);
@@ -106,5 +112,43 @@ class PublicKey
         }
 
         throw new Exception('Missing public key');
+    }
+
+    /**
+     * Register a callback that is for caching the response
+     *
+     * @param callable $callback
+     */
+    public static function cacheThrough(callable $callback)
+    {
+        static::$cacheThroughCallback = $callback;
+    }
+
+    /**
+     * @return ?HandlerStack
+     */
+    protected function buildCacheMiddlewareStack()
+    {
+        if (static::$cacheThroughCallback) {
+            return tap(HandlerStack::create(), function (HandlerStack $stack) {
+                $stack->push(call_user_func(static::$cacheThroughCallback), 'cache');
+            });
+        }
+
+        return null;
+    }
+
+    /**
+     * Build guzzle client options
+     * @return array
+     */
+    private function buildOptions()
+    {
+        $options = [];
+        if ($handler = $this->buildCacheMiddlewareStack()) {
+            $options['handler'] = $handler;
+        }
+
+        return $options;
     }
 }
